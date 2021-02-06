5ba7519663dde158e51524952648997df61f131e
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-06 15:20:48 -0600

    second item template added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import MainForm from "./MainForm"
 import MainTicket from "./MainTicket"
 import ItemTemplate from "./ItemTemplate"
+import ItemPairTemplate from "./ItemPairTemplate"
 
 function App() {
   return (
     <div className="App">
       <MainTicket/>
       <ItemTemplate/>
+      <ItemPairTemplate/>
       <MainForm/>
       
     </div>
---
+++ src/ItemPairTemplate.js
@@ -0,0 +1,11 @@
+import "./ItemPairTemplate.css"
+function ItemPairTemplate(){
+    return(
+        <div className="ItemPairContainer">
+            <div className="ItemPairDescription">Descripcion escrita</div>
+            <div className="ItemPairAmount">Monto escrito 20,500</div>
+        </div>
+    );
+}
+
+export default ItemPairTemplate
