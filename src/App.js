b00d536c34a6afd28cc6caba94a82a83e7f8961d
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-07 17:10:34 -0600

    pair item component deleted

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import MainForm from "./MainForm"
 import MainTicket from "./MainTicket"
 import ItemTemplate from "./ItemTemplate"
-import ItemPairTemplate from "./ItemPairTemplate"
 import Total from "./Total"
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
     <div className="App">
       <MainTicket/>
       <ItemTemplate/>
-      <ItemPairTemplate/>
       <Total/>
       <MainForm/>
       
