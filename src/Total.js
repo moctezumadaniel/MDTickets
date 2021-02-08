32cc1f950ff3caff40092bcad7d313d3d64e6cd5
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-08 15:28:15 -0600

    initial style of the total component

--- src/Total.js
+++ src/Total.js
@@ -1,9 +1,12 @@
 import "./Total.css"
 function Total (){
     return(
-        <div>
-            <div>Total</div>
-            <div>Monto Total 20,000</div>
+        <div className="ContainerTotalComponent">
+            <div className="ContainerTotal">
+                <div className="TicketTotalDescription">Total</div>
+                <div className="TicketTotalAmount">1,000,000.00,000
+</div>
+            </div>
         </div>
     );
 }
