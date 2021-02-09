01aa3a592669d3a15070fcb761474a7be34f44bd
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-09 15:25:21 -0600

    Main form styles added

--- src/MainForm.js
+++ src/MainForm.js
@@ -1,18 +1,12 @@
+import "./MainForm.css"
 function MainForm (){
     return(
         <div className="MainFormComponentContainer">
-            <form>
-                <label className="MainFormDesciptionContainer">
-                    NUEVA DESCRIPCION
-                    <textarea className="MainFormDescriptionArea"placeholder ="Escribe la descripcion del concepto"type="text" name="newDescription"/>
-                </label>
-                <label className="MainFormAmounthContainer">
-                    NUEVO MONTO
-                    <textarea className="MainFormAmounthArea" placeholder ="Escribe el monto del concepto"type="text" name="newAmount"/>
-                </label>
-            </form>
-            <button>AGREGAR</button>
-            <button>COMPARTIR</button>
+            <div className="MainFormContainer">
+                <textarea className="MainFormDescriptionArea"/>
+                <div className="MainFormSplit"></div>
+                <input type="number" className="MainFormAmounthArea"/>
+            </div>
         </div>
 
     );
