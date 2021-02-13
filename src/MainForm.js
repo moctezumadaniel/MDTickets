db096b8f41ee57fd9cd8612984eab64cc0dd07f5
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-12 19:37:38 -0600

    hooks added to the description and amounth in the main form

--- src/MainForm.js
+++ src/MainForm.js
@@ -1,11 +1,21 @@
 import "./MainForm.css"
+import React, { useState } from 'react';
+
 function MainForm (){
+    const [description,setDescription] = useState("")
+    const [amounth,setAmounth] = useState(0)
+    const handleDescriptionChange = event=>{
+        setDescription(event.target.value);
+    }
+    const handleAmounthChange = event =>{
+        setAmounth(event.target.value);
+    }
     return(
         <div className="MainFormComponentContainer">
             <div className="MainFormContainer">
-                <textarea className="MainFormDescriptionArea"/>
+                <textarea className="MainFormDescriptionArea" onChange={handleDescriptionChange} value={description}/>
                 <div className="MainFormSplit"></div>
-                <input type="number" className="MainFormAmounthArea"/>
+                <input type="number" className="MainFormAmounthArea" onChange={handleAmounthChange} value={amounth}/>
             </div>
         </div>
 
