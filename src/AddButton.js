f3859583df2daedd6f726025e3ce881b2867b6a3
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-15 16:43:53 -0600

    logic to add new object in the item reducer based on the current state of the description and amounth

--- src/AddButton.js
+++ src/AddButton.js
@@ -1,10 +1,24 @@
 import './AddButton.css'
+import { newItem } from './actions'
+import { useSelector, useDispatch } from 'react-redux';
+
 function AddButton (){
+    const description = useSelector(state => state.descriptionReducer)
+    const amounth = useSelector(state => state.amounthReducer)
+    const dispatch = useDispatch();
+    const handleAddPress = (newDescription, newAmounth) =>{
+        dispatch(newItem(newDescription, newAmounth));
+    }
+
     return(
         <div className="AddButtonContainer">
-            <button className="AddButton">ADD</button>
+
+            <button className="AddButton" 
+            onClick={handleAddPress(description,amounth)}>ADD</button>
+
         </div>
     )
+    
 }
 
 export default AddButton;
