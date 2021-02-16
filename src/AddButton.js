dcf8c19d1335df5efeae723fef854aada1c969b0
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-16 11:54:30 -0600

    logic to sow or hide component add button

--- src/AddButton.js
+++ src/AddButton.js
@@ -6,16 +6,15 @@ import { addConcept } from './actions'
 function AddButton (){
     const description = useSelector(state=>state.descriptionReducer);
     const amounth = useSelector(state=>state.amounthReducer)
-    const stateButton = useSelector(state=>state.addButtonReducer)
     const dispatch = useDispatch()
-    const handleAddItem = (description, amounth)=>{
+    function handleAddItem  (description, amounth, callback){
         dispatch(addConcept(description,amounth))
     }
     
 
     return(
         <div className="AddButtonContainer">
-            {stateButton ?
+            {description.length > 0 || amounth > 0 ?
                 <button className="AddButton"
                 onClick={()=>handleAddItem(description,amounth)}>ADD</button>
             :""}
