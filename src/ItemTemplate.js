43b74c5448b35c8f4892631296fa156c0582d811
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-19 16:41:18 -0600

    function to activate buttons edite and delete componenet

--- src/ItemTemplate.js
+++ src/ItemTemplate.js
@@ -1,17 +1,21 @@
 import "./ItemTemplate.css"
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { activateDeButtons } from './actions'
 function ItemTemplate(){
     const items = useSelector(state=>state.itemReducer)
-
+    const dispatch = useDispatch()
+    function handleItemClick() {
+        dispatch(activateDeButtons())
+    }
     return(<div>
 
         { items.map(i=>
         (
         <div className="MainItemTemplateContainer">
-            <div className="ItemTemplateContainer">
-                <div className="ItemTemplateDescription">{i.description}</div>
-                <div className="ItemTemplateSplit"></div>
-                <div className="ItemTemplateAmount">{i.amounth}</div>
+            <div className="ItemTemplateContainer" onClick={handleItemClick}>
+                <div className="ItemTemplateDescription" >{i.description}</div>
+                <div className="ItemTemplateSplit" ></div>
+                <div className="ItemTemplateAmount" >{i.amounth}</div>
             </div>
         </div>
         ))}
