6018380cd99e9735044dfdbf200a020f31c9821a
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-20 18:47:38 -0600

    handle item click corrected to work with param

--- src/ItemTemplate.js
+++ src/ItemTemplate.js
@@ -1,18 +1,19 @@
 import "./ItemTemplate.css"
 import { useSelector, useDispatch } from 'react-redux'
-import { activateDeButtons } from './actions'
+import { activateDeButtons, selectItem } from './actions'
 function ItemTemplate(){
     const items = useSelector(state=>state.itemReducer)
     const dispatch = useDispatch()
-    function handleItemClick() {
-        dispatch(activateDeButtons())
+    function handleItemClick(key) {
+        dispatch(activateDeButtons(),
+        dispatch(selectItem(key)))
     }
     return(<div>
 
         { items.map(i=>
         (
         <div className="MainItemTemplateContainer" key ={i.key}>
-            <div className="ItemTemplateContainer" onClick={handleItemClick}>
+            <div className="ItemTemplateContainer" onClick={()=>handleItemClick(i.key)}>
                 <div className="ItemTemplateDescription" >{i.description}</div>
                 <div className="ItemTemplateSplit" ></div>
                 <div className="ItemTemplateAmount">{i.amounth}</div>
