9dffc44bd4daef95c2dc9badae2c3e61a2c32772
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-21 19:41:54 -0600

    function to eliminate items without a numeric value from the grand total caculation

--- src/Total.js
+++ src/Total.js
@@ -3,9 +3,8 @@ import { useSelector } from 'react-redux'
 
 function Total (){
     const currentItems = useSelector(state => state.itemReducer)
-    const grandTotal = currentItems.reduce(function(accumulator,currentItem){
-        return accumulator + parseFloat(currentItem.amounth)
-    },0)
+    const itemsAmounths = currentItems.filter(i=>i.amounth !== "")
+    const grandTotal = itemsAmounths.reduce((accumulator, currentItem)=>accumulator + parseFloat(currentItem.amounth),0)
     return(
         <div className="ContainerTotalComponent">
             <div className="ContainerTotal">
