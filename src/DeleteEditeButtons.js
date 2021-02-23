27cc5e38b34d9237bdb429c304ce677ed9581f7c
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-23 15:33:46 -0600

    logic to pass the selected desciption and amounth to the main form

--- src/DeleteEditeButtons.js
+++ src/DeleteEditeButtons.js
@@ -1,17 +1,25 @@
 import "./DeleteEditeButtons.css"
 import { useSelector, useDispatch } from 'react-redux'
-import { deleteItem, deactivateDeButtons, hideItem } from './actions'
+import { deleteItem, deactivateDeButtons, hideItem, changeDescription, changeAmounth } from './actions'
 function DeleteEditeButtons (){
     const showButtons = useSelector(state=>state.deleteEditButtonsReducer)
     const currentKey = useSelector(state=>state.itemSelectionReducer)
+    const allItems = useSelector(state=>state.itemReducer)
     const dispatch = useDispatch()
     const handleDelete = (key) =>{
         dispatch(deleteItem(key),
         dispatch(deactivateDeButtons()))
     }
     const handleEdit = (key)=>{
+        let item = allItems.filter((i)=>i.key===currentKey)
+        let description = item[0].description
+        let amounth = item[0].amounth
+
         dispatch(hideItem(key),
-        dispatch(deactivateDeButtons()))
+        dispatch(deactivateDeButtons()),
+        dispatch(changeDescription(description)),
+        dispatch(changeAmounth(amounth)))
+        console.log(allItems)
     }
     return(
     <div>
--- src/ItemTemplate.js
+++ src/ItemTemplate.js
@@ -24,6 +24,7 @@ function ItemTemplate(){
             </div>
         </div>
         ):
+
         (
             <div className="MainItemTemplateContainer" key ={i.key}>
             <div className="ItemTemplateContainer" onClick={()=>handleItemClick(i.key)}>
