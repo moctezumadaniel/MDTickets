9120706f00c5709b332437ee93bd9a946fa54a5f
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-24 12:20:29 -0600

    logic to hide edite delete buttons when an item is in edition

--- src/DeleteEditeButtons.js
+++ src/DeleteEditeButtons.js
@@ -5,6 +5,7 @@ function DeleteEditeButtons (){
     const showButtons = useSelector(state=>state.deleteEditButtonsReducer)
     const currentKey = useSelector(state=>state.itemSelectionReducer)
     const allItems = useSelector(state=>state.itemReducer)
+    const editStatus = useSelector(state=>state.editItemStatus)
     const dispatch = useDispatch()
     const handleDelete = (key) =>{
         dispatch(deleteItem(key),
@@ -23,7 +24,7 @@ function DeleteEditeButtons (){
     }
     return(
     <div>
-        {showButtons ?
+        {showButtons && !editStatus ?
         <div className="DeleteEditeButtonsContainer">
             <button className="EditeButton" onClick={()=>{handleEdit(currentKey)}}>EDIT</button>
             <button className="DeleteButton" onClick={()=>{handleDelete(currentKey)}}>DELETE</button>
