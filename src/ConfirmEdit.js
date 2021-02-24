dcb8616d9096f3e2d180ce03d0696ddc421c8deb
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-24 10:25:41 -0600

    usage of edit status to render confirm edition button

--- src/ConfirmEdit.js
+++ src/ConfirmEdit.js
@@ -1,12 +1,16 @@
 import './ConfirmEdit.css'
+import { useSelector } from 'react-redux'
+
+
 function ConfirmEditing (){
+    const editStatus = useSelector(state=>state.rootReducers)
     return(
         <div>
-            {
+            {editStatus ?
             <div>
             <button>CONFIRM</button>
             </div>
-            }
+            :""}
             
         </div>
     );
